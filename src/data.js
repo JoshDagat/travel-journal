242a85e2a8bdb8e12cfed40f57e189d6ef4e25e8
JosuerBague <josuer.online.work@gmail.com>
2022-07-05 21:41:54 +0800

    Populated data.js

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        title: "Changi Airport",
+        location: "Singapore",
+        googleMapsUrl: "https://goo.gl/maps/VSJPvkXSraAnErDK8",
+        startDate: "12 Jan, 2019",
+        endDate: "13 Jan, 2019",
+        description: "Singapore Changi Airport, commonly known as Changi Airport, is a major civilian international airport that serves Singapore, and is one of the largest transportation hubs in Asia.",
+        imageUrl: "https://unsplash.com/photos/O-Unw-ho1kQ"
+    },
+    {
+        title: "Pattaya City",
+        location: "Thailand",
+        googleMapsUrl: "https://goo.gl/maps/LBqhAxD5bFHymLmj8",
+        startDate: "14 Jan, 2019",
+        endDate: "15 April, 2019",
+        description: "Pattaya is a city on Thailand’s eastern Gulf coast known for its beaches. A quiet fishing village as recently as the 1960s, it’s now lined with resort hotels, high-rise condos, shopping malls, cabaret bars and 24-hour clubs. Nearby, hillside Wat Phra Yai Temple features an 18m-tall golden Buddha. The area also features several designer golf courses, some with views of Pattaya Bay.",
+        imageUrl: "https://www.google.com/maps/place/Pattaya+City,+Bang+Lamung+District,+Chon+Buri+20150,+Thailand/@12.9235557,100.8824551,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipNE8f_h5nJCjKGGCa_lg0Bb0IEN9FGK2_qvhm-k!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNE8f_h5nJCjKGGCa_lg0Bb0IEN9FGK2_qvhm-k%3Dw152-h86-k-no!7i1916!8i1078!4m5!3m4!1s0x310295924d31d7e3:0xa97ff301a9bd10ff!8m2!3d12.9235557!4d100.8824551"
+    },
+    {
+        title: "Sattahip",
+        location: "Thailand",
+        googleMapsUrl: "https://goo.gl/maps/j76fjqNyG35ZG45B6",
+        startDate: "14 Jan, 2019",
+        endDate: "15 April, 2019",
+        description: "Sattahip is a mix of popular tourist destination, fishing town and large naval base on Thailand's eastern coastline and overlooking the Bay of Bangkok.",
+        imageUrl: "https://www.google.com/maps/place/Sattahip,+Sattahip+District,+Chon+Buri,+Thailand/@12.6365145,100.9263886,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipOuDqUEnEBkmwgpkOvoDUb4jXvlnbfgHIjvR0ol!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipOuDqUEnEBkmwgpkOvoDUb4jXvlnbfgHIjvR0ol%3Dw114-h86-k-no!7i1477!8i1108!4m5!3m4!1s0x31028c0f9a6924a9:0x2892ba245b9ccdfd!8m2!3d12.6736927!4d100.895357#"
+    }
+]
