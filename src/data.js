0447f772dc6b6e0ebaf83008edac5860d2ce0331
JosuerBague <josuer.online.work@gmail.com>
2022-07-06 18:29:14 +0800

    Modified imageUrl properties from data objects.

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ export default [
         description: "Singapore Changi Airport, commonly known as Changi Airport, is a major civilian international airport that serves Singapore, and is one of the largest transportation hubs in Asia.",
         // imageUrl: 'https://ibb.co/vH3qX8L'
         // imageUrl: "https://postimg.cc/Mvxz1bcx"
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        imageUrl: "./images/changi-airport.jpg"
     },
     {
         title: "Pattaya City",
@@ -17,7 +17,7 @@ export default [
         startDate: "14 Jan, 2019",
         endDate: "15 April, 2019",
         description: "Pattaya is a city on Thailand’s eastern Gulf coast known for its beaches. A quiet fishing village as recently as the 1960s, it’s now lined with resort hotels, high-rise condos, shopping malls, cabaret bars and 24-hour clubs. Nearby, hillside Wat Phra Yai Temple features an 18m-tall golden Buddha. The area also features several designer golf courses, some with views of Pattaya Bay.",
-        imageUrl: "https://ibb.co/Bw5v6gD"
+        imageUrl: "./images/pattaya.jpg"
     },
     {
         title: "Sattahip",
@@ -26,6 +26,6 @@ export default [
         startDate: "14 Jan, 2019",
         endDate: "15 April, 2019",
         description: "Sattahip is a mix of popular tourist destination, fishing town and large naval base on Thailand's eastern coastline and overlooking the Bay of Bangkok.",
-        imageUrl: "https://ibb.co/Gkvv6wv"
+        imageUrl: "./images/sattahip.jpg"
     }
 ]
