6a6055f390d2c02171882db8b92239ab042492f7
JosuerBague <josuer.online.work@gmail.com>
2022-07-06 22:51:36 +0800

    Create src/components/navbar fodler and moved releavant items inside.

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className='navbar'>
+            <img src='../assets/images/Globe.svg'></img>
+            <h1>My Travel Journal</h1>
+        </nav>
+    )
+}
