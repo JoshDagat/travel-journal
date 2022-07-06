7c1ad181a90465c28d400305756f35daccf1c2bf
JosuerBague <josuer.online.work@gmail.com>
2022-07-06 23:21:23 +0800

    Finalized Application css.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Destination from './components/destination/Destination';
+import Navbar from './components/navbar/Navbar';
 import data from './data.js'
+import './App.css'
 
 export default function App() {
+
+    const destinations = data.map(location => {
+        return (
+            <Destination key={location.title} {...location} />
+        )
+    })
+
     return (
-        <Destination {...data[0]} />
+        <div className='app'>
+            <Navbar />
+            <div className='app__main-content'>
+                {destinations}
+            </div>
+        </div>
     )
 }
