9cfa56e35061ec71e5ae19efd3a1d0688a1850a0
JosuerBague <josuer.online.work@gmail.com>
2022-07-06 22:48:19 +0800

    Finished styling Destination component

--- src/components/destination/Destination.js
+++ src/components/destination/Destination.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
-import './Destination.css'
+import './Destination.css';
+
 
 export default function Destination(props) {
     console.log(props.imageUrl)
@@ -9,9 +10,11 @@ export default function Destination(props) {
         <article className='destination'>
             <img className='destination__image' src={props.imageUrl} alt={props.title} />
             <div className='destination__details'>
-                <FontAwesomeIcon icon={solid('location-dot')} />
-                <span className='destination__country'>{props.location}</span>
-                <a className='destination__maps' href={props.googleMapsUrl}>View on Google Maps</a>
+                <div className='destination__locate'>
+                    <FontAwesomeIcon className='destination__icon' icon={solid('location-dot')} />
+                    <span className='destination__country'>{props.location.toUpperCase()}</span>
+                    <a className='destination__g-maps' href={props.googleMapsUrl}>View on Google Maps</a>
+                </div>
                 <h1 className='destination__name'>{props.title}</h1>
                 <span className='destination__travel-date'>{props.startDate} - {props.endDate}</span>
                 <p className='destination__description'>{props.description}</p>
